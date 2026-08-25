4bfb8da175289bee575d3998317828512312806b
Victor Lira <victorliradeveloper@gmail.com>
2026-08-24 23:25:33 -0300

    chore: update links

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,5 +1,5 @@
-export const GITHUB_LINk = 'https://github.com/victorlirafront';
-export const LINKEDIN_LINK = 'https://www.linkedin.com/in/victor-lira-front-end/';
+export const GITHUB_LINk = 'https://github.com/';
+export const LINKEDIN_LINK = 'https://www.linkedin.com/in/';
 export const NEXT_LINK = 'https://nextjs.org/';
 export const NODE_LINK = 'https://nodejs.org/';
 export const MYSQL_LINK = 'https://www.mysql.com/';
